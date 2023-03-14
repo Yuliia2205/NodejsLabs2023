8b6f71846748f9e33dcf9af15ecd2ad97451e2cf
HootEgor <egor.hoot@gmail.com>
2023-03-14 15:34:07 +0100

    Lab1

--- lab1.js
+++ lab1.js
@@ -29,6 +29,67 @@ function pigIt(str) {
     });
     return pigLatinWords.join(" ");
 }
+
 console.log("\nMoving the first letter of each word to the end of it, then add ay to the end of the word:")
 console.log(pigIt('Pig latin is cool')); // igPay atinlay siay oolcay
 console.log(pigIt('Hello world !'));     // elloHay orldway !
+
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+function isPalindrome(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    const numStr = x.toString();
+    const length = numStr.length;
+
+    for (let i = 0; i < length / 2; i++) {
+        if (numStr[i] !== numStr[length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log("\nx is a palindrome:")
+console.log(isPalindrome(31413));
+console.log(isPalindrome(314413));
+console.log(isPalindrome(42));
+console.log(isPalindrome(-42));
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+// An input string is valid if:
+//
+// Open brackets must be closed by the same type of brackets.
+//     Open brackets must be closed in the correct order.
+//     Every close bracket has a corresponding open bracket of the same type.
+
+function isValid(s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s.charAt(i);
+
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else if (char === ')' && stack.length > 0 && stack[stack.length - 1] === '(') {
+            stack.pop();
+        } else if (char === '}' && stack.length > 0 && stack[stack.length - 1] === '{') {
+            stack.pop();
+        } else if (char === ']' && stack.length > 0 && stack[stack.length - 1] === '[') {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log("\nBrackets:")
+console.log(isValid("{()}[]"))
+console.log(isValid("{([])}"))
+console.log(isValid("([])}"))
